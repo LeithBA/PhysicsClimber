022fef3c58843af2ef2a7376671d99f669e1bf61
Mehtap Kurucay <mehtap.kurucay@ue-germany.de>
2019-04-18 18:48:00 +0200

    Added: Very Basic Movement Need to : improve movement feeling, Implement Jump on Space,better aesthetics

---
+++ Assets/Mehtap/Scripts/Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rigid;
+    public GameObject player;
+    private Vector2 moveVelocity;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        rigid.MovePosition(rigid.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
