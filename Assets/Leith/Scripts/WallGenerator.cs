4a897d513902fbf113d2834f0c7f9b72eccb0703
LeithBA <leithbenabdessalem@gmail.com>
2019-04-19 16:12:35 +0200

    Lava Eraser Added, Background(test) added

--- Assets/Leith/Scripts/WallGenerator.cs
+++ Assets/Leith/Scripts/WallGenerator.cs
@@ -11,7 +11,7 @@ public class WallGenerator : MonoBehaviour
 
     private void Start()
     {
-		wallHeight = walls.transform.GetChild(0).localScale.y;
+		wallHeight = walls.transform.GetChild(0).localScale.y + 1;
         nextLevelHeight = wallHeight * currentLevel;
     }
     void Update()
@@ -19,9 +19,14 @@ public class WallGenerator : MonoBehaviour
         Debug.Log(nextLevelHeight + ", " + currentLevel);
         if (this.transform.position.y >= nextLevelHeight - wallHeight)
         {
-            Instantiate(walls, new Vector3(0, nextLevelHeight, 0), Quaternion.identity, wallContainer.transform);
+			GameObject newWall =  Instantiate(walls, new Vector3(0, nextLevelHeight, 0), Quaternion.identity, wallContainer.transform);
+			newWall.name = "Level (" + currentLevel + ")";
             currentLevel++;
             nextLevelHeight = walls.transform.GetChild(0).localScale.y * currentLevel;
+			if(currentLevel > 2)
+			{
+				Destroy(wallContainer.transform.GetChild(0).gameObject);
+			}
         }
     }
 }
