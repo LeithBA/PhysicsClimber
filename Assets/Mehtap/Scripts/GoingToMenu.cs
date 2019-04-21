1605d9e97fef81ef15c36e4ca35370ec58a33399
Mehtap Kurucay <mehtap.kurucay@ue-germany.de>
2019-04-21 16:35:28 +0200

    Added: Menu and Credits

---
+++ Assets/Mehtap/Scripts/GoingToMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GoingToMenu : MonoBehaviour
+{
+    public Button goingBack;
+    public string ToMenu; 
+
+    public void GoingBack()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
