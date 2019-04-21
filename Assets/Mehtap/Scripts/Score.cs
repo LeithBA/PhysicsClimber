27ce82d61d60bb77d824e2c628b76cf6a2d46f46
LeithBA <leithbenabdessalem@gmail.com>
2019-04-21 16:38:42 +0200

    Common Folder added, GameScene added

--- Assets/Mehtap/Scripts/Score.cs
+++ Assets/Mehtap/Scripts/Score.cs
@@ -9,16 +9,22 @@ public class Score : MonoBehaviour
     float maxPos;
     public Text score;
 
+    LavaVulnerability LV;
+
+    void Start()
+    {
+        LV = GetComponent<LavaVulnerability>();
+    }
     void Update()
     {
         float playerPos = gameObject.transform.position.y;
 
-        if (playerPos > maxPos)
+        if (playerPos > maxPos && LV.dead == false)
         {
             maxPos = playerPos;
         }
 
         //Mathf.Round -> Not to get 1.4 or sth similar
-        score.text = "Score:" + Mathf.Round(maxPos).ToString();
+        score.text = "Score:" + Mathf.Round(maxPos * 10).ToString();
     }
 }
