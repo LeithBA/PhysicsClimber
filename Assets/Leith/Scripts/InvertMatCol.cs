4eb331a24ef92fa07d0ff166ef690f5468b48524
TappyDiTap <mehtap.kurucay@hotmail.de>
2019-04-22 10:04:28 +0200

    Fixed: Bug of Invert Main Menue Still to do regarding Menu: Invert Text, too ||Music mute

--- Assets/Leith/Scripts/InvertMatCol.cs
+++ Assets/Leith/Scripts/InvertMatCol.cs
@@ -8,25 +8,26 @@ public class InvertMatCol : MonoBehaviour
     public bool dark;
     [SerializeField] public Color32 envDark, envLight, objDark, objLight, bgDark, bgLight;
 
-    public void invert()
+    public void Invert()
     {
-        if (dark == true)
+        dark = !dark; // Tap was here~
+
+        /*if (dark == true)
         {
             //MAKE LIGHT
             dark = false;
         }
 
-        if (dark == false)
+        if (dark == false) // Should be else if here
         {
             //MAKE DARK
             dark = true;
-        }
+        }*/
     }
 
 
-    public void Update()
+    public void InvertStuff()
     {
-
         if (dark == true)
         {
             Camera.main.backgroundColor = bgLight;
@@ -39,12 +40,16 @@ public class InvertMatCol : MonoBehaviour
 
         else if (dark == false)
         {
+
             Camera.main.backgroundColor = bgDark;
             for (int i = 0; i < GameObject.Find("MenuManager").GetComponent<MainMenu>().texts.Length; i++)
             {
                 GameObject.Find("MenuManager").GetComponent<MainMenu>().texts[i].color = objLight;
 
             }
+         
         }
+        Invert();
+        Debug.Log(dark);
     }
 }
